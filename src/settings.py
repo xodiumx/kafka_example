bb75afab9149464c6d9625f7fc72001152fc3cb2
Maks <am.xlexeev@gmail.com>
2024-07-28 19:42:08 +0500

    init

---
+++ src/settings.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    KAFKA_HOSTS: list[str]
+    KAFKA_SASL_PLAIN_USERNAME: str
+    KAFKA_SASL_PLAIN_PASSWORD: str
+    KAFKA_MAX_REQUEST_SIZE: int
+    KAFKA_FETCH_MESSAGE_MAX_BYTES: int
+    KAFKA_COMPRESS: int
+    KAFKA_TOPIC_PREFIX: str
+
+    PYTHONPATH: str
+
+    class Config:
+        env_file = '.env.example'
+        env_file_encoding = 'utf-8'
+
+
+settings = Settings()
