0437d0466493ed48b28c1b192025bb6a2f40110f
Maks <am.xlexeev@gmail.com>
2024-10-05 16:55:05 +0300

    update docstrings

--- src/kafka_service_example/services/admin.py
+++ src/kafka_service_example/services/admin.py
@@ -7,12 +7,25 @@ class KafkaAdminService(KafkaAdminBase):
     def get_topics(self, topic_name: str | None = None, timeout: float = 1) -> TopicsInfo:
         """
         Get information about all topics or specific topic
+
+        Parameters:
+        topic_name: topic name by default is None
+                    if None get info about all topics
+        timeout: float num, how long to expect a response
         """
         return TopicsInfo(topics=self._get_topics_info(topic_name=topic_name, timeout=timeout))
 
     def create_new_topics(
         self, topics_for_create: list, partitions_count: int, repliacation_factor: int
     ) -> CreatedTopics:
+        """
+        Create new topics
+
+        Parameters:
+            topics_for_create: list of topics name
+            partitions_count: num of topic partiotions
+            repliacation_factor: num of replication factor
+        """
         return CreatedTopics(
             **self._create_new_topics(
                 topics_for_create=topics_for_create,
@@ -21,7 +34,13 @@ class KafkaAdminService(KafkaAdminBase):
             )
         )
 
-    def delete_old_topics(self, topics_for_delete) -> DeletedTopics:
+    def delete_old_topics(self, topics_for_delete: list[str]) -> DeletedTopics:
+        """
+        Delete topics by names
+
+        Parameters:
+            topics_for_delete - list of topics name
+        """
         return DeletedTopics(**self._delete_old_topics(topics_for_delete=topics_for_delete))
 
     def update_partitions(self): ...
